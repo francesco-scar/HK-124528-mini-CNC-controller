4b2e487b2ca85ca40abbbdf7d1e6bf47a67b48ca
Francesco S <36336996+francesco-scar@users.noreply.github.com>
2021-03-03 20:57:18 +0000

    :hammer: Filename modification

--- Duplicate_line_gcode.py
+++ Duplicate_line_gcode.py
@@ -8,7 +8,12 @@ file = open(sys.argv[1], 'r')
 lines = file.read().split('\n')
 file.close()
 
-new_file = open(sys.argv[1]+'.edit', 'w')
+if len(sys.argv[1].split('/')) == 1:
+	output_filename = 'Edit_' + sys.argv[1]
+else:
+	output_filename = sys.argv[1].split(sys.argv[1].split('/')[-1])[0] + 'Edit_' + sys.argv[1].split('/')[-1]
+
+new_file = open(output_filename, 'w')
 for line in lines:
 	new_file.write((line+'\n')*int(sys.argv[2]))
 new_file.close()
